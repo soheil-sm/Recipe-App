40a706066277420d97eb38e182fff6736d5e7897
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-03-15 01:41:40 +0330

    Initial commit

---
+++ src/main/java/org/example/recipie/RecipieApplication.java
@@ -0,0 +1,13 @@
+package org.example.recipie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recipie/RecipieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recipie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
