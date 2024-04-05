83871986abc38e6f4c017bb088e1564b5d35932c
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-04-05 22:35:19 +0330

    Add test for Recipe Service class

--- src/test/java/org/example/recipie/service/RecipeServiceImplTest.java
+++ src/test/java/org/example/recipie/service/RecipeServiceImplTest.java
@@ -1,4 +1,42 @@
+package org.example.recipie.service;
+
+import org.example.recipie.domain.Recipe;
+import org.example.recipie.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 public class RecipeServiceImplTest {
-  
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
 }
